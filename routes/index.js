b9bcd6ef6ca058f51e230557465f72249cba0a80
Juan Martin Cechetto <juan.cechetto@bigbox.com.ar>
2021-11-25 21:01:27 -0300

    add login

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 import express from 'express';
-import { Contenedor } from "../contenedor.js";
-import mysqlOpt from "../options/mysql.js"
+import Container from "../mongoContainer.js";
+import product from "../models/product.js"
 
 const { Router } = express;
 
-const mysql = new Contenedor(mysqlOpt, 'products')
+const mongo = new Container(product)
 const router = new Router();
 
 router.get('/', async (req, res) => {
-  const data = await mysql.getAll();
+  const data = await mongo.findAll();
   res.json(data);
 });
 
 router.post('/', async (req, res) => {
   try {
-    await mysql.save(req.body);
+    await mongo.create(req.body);
     res.status(200).json({success: 'ok'});
   } catch (error) {
     res.status(500).json({error: 'cannot insert into db'});
